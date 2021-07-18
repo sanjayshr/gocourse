9a35a296cc15ca23f5164e69f376fe88c29ed99a
Sanjay Shiradwade <sanjayshiradwade@gmail.com>
2021-07-18 17:15:37 +0530

    named fields

--- utils/perimeter_test.go
+++ utils/perimeter_test.go
@@ -15,17 +15,21 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 	areaTests := []struct {
+		name  string
 		shape Shape
 		want  float64
 	}{
-		{Rectangle{10.0, 20.0}, 200}, {Circle{10}, 314.1592653589793},
+		{name: "Rectangle", shape: Rectangle{10.0, 20.0}, want: 200}, {name: "Circle", shape: Circle{10}, want: 314.1592653589793},
 	}
 
 	for _, tt := range areaTests {
-		got := tt.shape.Area()
-		if got != tt.want {
-			t.Errorf("got %g want: %g", got, tt.want)
-		}
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.want {
+				t.Errorf("got %g want: %g", got, tt.want)
+			}
+
+		})
 	}
 
 	//	checkArea := func(t testing.TB, shap Shape, want float64) {
